d68c0b5d77cce979caab3062aecd36416a676b90
Monraa <ramon.hv03@gmail.com>
2023-02-01 14:11:10 +0100

    Update Main.java
    Nuevo bucle for para mostrar un contador de 1-10

--- testmodule/src/Main.java
+++ testmodule/src/Main.java
@@ -5,9 +5,13 @@ public class Main {
          System.out.println("Hello world!");
      }
 
+     int contador=0;
+    for (int i=0;i<10;i++){
+        contador++;
+        System.out.println(contador);
+    }
 
-
-
+        System.out.println("Contador final= "+contador);
 
 
 
