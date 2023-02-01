3447becbf49c1051e18f0d1abdd168620baf08a9
Monraa <ramon.hv03@gmail.com>
2023-02-01 14:15:16 +0100

    Update Main.java
    cambiar elprimer hola mundo a 25 repeticiones

--- testmodule/src/Main.java
+++ testmodule/src/Main.java
@@ -1,14 +1,17 @@
 public class Main {
     public static void main(String[] args) {
 
-     for (int i=0;i<10;i++){
+     for (int i=0;i<20;i++){
          System.out.println("Hello world!");
      }
-
+    int a=0;
      int contador=0;
     for (int i=0;i<10;i++){
         contador++;
         System.out.println(contador);
+        if (contador==8){
+             a=1;
+        }
     }
 
         System.out.println("Contador final= "+contador);
